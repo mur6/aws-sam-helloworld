f13b48d8b68344c679a473db305265e4959c0db1
MURAKI Taichi <muraki.taichi@gmail.com>
2018-03-14 14:16:26 +0900

    Added second handler.

--- lambda_function.py
+++ lambda_function.py
@@ -3,10 +3,11 @@ import json
 print('Loading function')
 
 
-def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
-    print("value1 = " + event['key1'])
-    print("value2 = " + event['key2'])
-    print("value3 = " + event['key3'])
-    return event['key1']  # Echo back the first key value
-    #raise Exception('Something went wrong')
+def lambda_handler1(event, context):
+    print("Received event: " + json.dumps(event))
+    return {"body": "handler 1"}
+
+
+def lambda_handler2(event, context):
+    print("Received event: " + json.dumps(event))
+    return {"body": "handler 2"}
